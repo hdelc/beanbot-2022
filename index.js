59b0395b227f2177ee334bbbbb4540b17160b8fd
Carson <crtmusser@gmail.com>
2022-02-11 22:57:39 -0500

    Added pong

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ const config = require("./config.json");
 
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]})
 
+const prefix = "##";
+
+client.on("messageCreate" , function(message) {
+	if(message.author.bot) return;
+	if(!message.content.statrsWith(prefix)) return;
+
+	const commandBody = message.content.silce(prefix.length);
+	const args = commandBody.split(' ');
+	const command = args.shift().toLowerCase();
+
+	if(command === "ping"){
+	message.reply("pong");
+	}
+
+});
 client.login(config.BOT_TOKEN)
