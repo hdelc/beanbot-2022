7742805ffc0ade13e5726e39633287dfd98af822
Carson <crtmusser@gmail.com>
2022-02-12 19:50:39 -0500

    Improved wordle

--- commands/wordle.js
+++ commands/wordle.js
@@ -10,7 +10,7 @@ data: new SlashCommandBuilder()
 			.setRequired(true)),
 	async execute(interaction) {
 	const wordle = "query"
-	const wLetters = worlde.split('');
+	const wLetters = wordle.split('');
 	const string = interaction.options.getString('input');
 	const letters = string.split('');
 	const intArray = intArrayMap(letters,wLetters);
@@ -22,7 +22,7 @@ data: new SlashCommandBuilder()
 function intArrayMap(list1, list2){
 var intArray = new Array(5)
 for(var i = 0; i<5; i++) {
-	if(list2.includes(list1[i]){
+	if(list2.includes(list1[i])){
 		if(list1[i] === list2[i]){
 			intArray[i] = 1;
 		} else {
@@ -41,11 +41,11 @@ function stringMap(list1, list2) {
 	var stringArray = new Array(5);
 	for(var i = 0; i<5; i++) {
 		if(list2[i] === 1) {
-			stringArray = str1.concat(list1[i],str1);
+			stringArray[i] = str1.concat(list1[i],str1);
 		} else if(list2[i] == -1) {
-			stringArray = str2.concat(list1[i],str2);
+			stringArray[i] = str2.concat(list1[i],str2);
 		} else {
-			stringArray = list1[i];
+			stringArray[i] = list1[i];
 		}
 	}
 	return stringArray;
